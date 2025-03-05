67e7b687a4c440984894582146ea92d9cafcf6f0
Jaya Sabeen <jayasabeen@Jayas-MacBook-Pro.local>
2025-03-05 16:14:36 +0530

    Initial Commit

---
+++ MovieList/MovieListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieListApp.swift
+//  MovieList
+//
+//  Created by Jaya Sabeen on 05/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MovieListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
